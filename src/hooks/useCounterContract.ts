90510581fd1d9260ef54d25e48aa3804ef41feae
HumanHighQualityMan <178885674@qq.com>
2024-07-10 11:08:52 +0800

    update code

--- src/hooks/useCounterContract.ts
+++ src/hooks/useCounterContract.ts
@@ -8,35 +8,36 @@ import { useQuery } from "@tanstack/react-query";
 import { CHAIN } from "@tonconnect/protocol";
 
 export function useCounterContract() {
-  const { client } = useTonClient();
-  const { sender, network } = useTonConnect();
+    const { client } = useTonClient();
+    const { sender, network } = useTonConnect();
 
-  const counterContract = useAsyncInitialize(async () => {
-    if (!client) return;
-    const contract = new Counter(
-      Address.parse(
-        network === CHAIN.TESTNET
-          ? "EQCb-P8ZUO8fBTym_gPd02T5S97k7JVjZXkaIc5q-09I05-w"
-          : "EQCb-P8ZUO8fBTym_gPd02T5S97k7JVjZXkaIc5q-09I05-w"
-      ) // replace with your address from tutorial 2 step 8
+    const counterContract = useAsyncInitialize(async () => {
+        if (!client) return;
+        const contract = new Counter(
+            Address.parse(
+                network === CHAIN.TESTNET
+                    ? "EQCb-P8ZUO8fBTym_gPd02T5S97k7JVjZXkaIc5q-09I05-w"
+                    : "EQCb-P8ZUO8fBTym_gPd02T5S97k7JVjZXkaIc5q-09I05-w"
+            ) // replace with your address from tutorial 2 step 8
+        );
+        return client.open(contract) as OpenedContract<Counter>;
+    }, [client]);
+
+    const { data, isFetching } = useQuery(
+        ["counter"],
+        async () => {
+            console.log(counterContract)
+            if (!counterContract) return null;
+            return (await counterContract!.getCounter()).toString();
+        },
+        { refetchInterval: 3000 }
     );
-    return client.open(contract) as OpenedContract<Counter>;
-  }, [client]);
 
-  const { data, isFetching } = useQuery(
-    ["counter"],
-    async () => {
-      if (!counterContract) return null;
-      return (await counterContract!.getCounter()).toString();
-    },
-    { refetchInterval: 3000 }
-  );
-
-  return {
-    value: isFetching ? null : data,
-    address: counterContract?.address.toString(),
-    sendIncrement: () => {
-      return counterContract?.sendIncrement(sender);
-    },
-  };
+    return {
+        value: isFetching ? null : data,
+        address: counterContract?.address.toString(),
+        sendIncrement: () => {
+            return counterContract?.sendIncrement(sender);
+        },
+    };
 }
