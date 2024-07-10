5a4d3c794fa2a957e5aaf68409a360d8e4c3c82b
HumanHighQualityMan <45343063+HumanHighQualityMan@users.noreply.github.com>
2024-07-10 03:06:20 +0000

    Deploying to gh-pages from @ HumanHighQualityMan/Ton-TMA-Wallet@247d9e9a334ac1b200657e5c4762e4c41721e99c 🚀

--- assets/index-dd17cd90.js
+++ assets/index-dd17cd90.js
@@ -1198,4 +1198,4 @@ Values:
 `,xce=os.div`
   max-width: 900px;
   margin: 0 auto;
-`;function Mce(){const{network:e}=Au();return console.log(qo),Xt(wce,{children:Xt(xce,{children:_i(sg,{children:[_i(fu,{children:[Xt(_B,{}),Xt(ug,{children:e?e===qo.TESTNET?"mainnet":"testnet":"N/A"})]}),Xt(pce,{}),Xt(bce,{}),Xt(yce,{})]})})})}const _ce="https://ton.org/testnet-global.config.json",Sce=new DU({defaultOptions:{queries:{refetchOnWindowFocus:!1}}});y4.createRoot(document.getElementById("root")).render(Xt(LZ,{manifestUrl:_ce,children:Xt(iD,{client:Sce,children:Xt(Mce,{})})}));
+`;function Mce(){const{network:e}=Au();return console.log(qo),Xt(wce,{children:Xt(xce,{children:_i(sg,{children:[_i(fu,{children:[Xt(_B,{}),Xt(ug,{children:e?e===qo.TESTNET?"mainnet":"testnet":"N/A"})]}),Xt(pce,{}),Xt(bce,{}),Xt(yce,{})]})})})}const _ce="https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json",Sce=new DU({defaultOptions:{queries:{refetchOnWindowFocus:!1}}});y4.createRoot(document.getElementById("root")).render(Xt(LZ,{manifestUrl:_ce,children:Xt(iD,{client:Sce,children:Xt(Mce,{})})}));
